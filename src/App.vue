8ce435c8d5d4bbd4416362bc827dd4110e72da44
prateekj117 <prateekj1171998@gmail.com>
2018-12-30 04:04:13 +0530

    Setting up items in Toolbar.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <v-app dark>
     <v-navigation-drawer app v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -17,9 +17,9 @@
       <v-toolbar-title>AMU-OSS</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View Meetups
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -33,7 +33,14 @@
 export default {
   data () {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        { icon: 'supervisor_account', title: 'View Meetups' },
+        { icon: 'room', title: 'Organize Meetup' },
+        { icon: 'person', title: 'Profile' },
+        { icon: 'face', title: 'Sign Up' },
+        { icon: 'lock_open', title: 'Sign in' }
+      ]
     }
   }
 }
