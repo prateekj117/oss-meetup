2ad7ae6d8e91979a66e6c36f55dadcda35e68dc0
prateekj117 <prateekj1171998@gmail.com>
2018-12-30 13:16:21 +0530

    Setting up different components

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container grid-list-xs text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-btn large router to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-btn large router to="/meetup/new" class="primary">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
