293c103ebb6d3ba895cd186dc45141f46e4d058c
prateekj117 <prateekj1171998@gmail.com>
2019-01-11 19:47:40 +0530

    Add CreateMeetup Page

--- src/store.js
+++ src/store.js
@@ -15,10 +15,23 @@ export default new Vuex.Store({
     }
   },
   mutations: {
-
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
   },
   actions: {
-
+    createMeetup ({ commit }, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'sdcdc'
+      }
+      // Reach out to firebase and store it
+      commit('createMeetup', meetup)
+    }
   },
   getters: {
     loadedMeetups (state) {
