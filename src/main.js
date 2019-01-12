4bc0e0c9d86fac41b346a682ddb532615445946a
prateekj117 <prateekj1171998@gmail.com>
2019-01-12 23:43:08 +0530

    Initialize firebase and add sign up form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import * as firebase from 'firebase'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
@@ -13,5 +14,14 @@ Vue.filter('date', DateFilter)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBw-i5V9BpwBFSijY0nr_17EzspTWaJkL0',
+      authDomain: 'oss-meetup.firebaseapp.com',
+      databaseURL: 'https://oss-meetup.firebaseio.com',
+      projectId: 'oss-meetup',
+      storageBucket: 'oss-meetup.appspot.com'
+    })
+  }
 }).$mount('#app')
